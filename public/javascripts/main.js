920898266aff2a6b34a57f49974816f5bf5fe0cb
Elliott Eggleston <ejegg@ejegg.com>
2014-09-30 17:29:50 -0700

    Whitespace!

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
 	app = angular.module('B', []);
 	app.controller('rangeAgeController', function($scope) {
 		this.range = {
@@ -20,8 +20,8 @@ $(function () {
 		subdomains: '1234'
 	});
 	var map;
-	$.getJSON('geodata/tracts2010.json', function (data) {
-		var onEachFeature = function (feature, layer) {
+	$.getJSON('geodata/tracts2010.json', function(data) {
+		var onEachFeature = function(feature, layer) {
 			var defaultStyle = {
 				color: "#2262CC",
 				weight: 1,
@@ -36,9 +36,9 @@ $(function () {
 				fillOpacity: 0.65,
 				fillColor: '#2262CC'
 			};
-			(function (layer, properties) {
+			(function(layer, properties) {
 				// Create a mouseover event
-				layer.on("mouseover", function (e) {
+				layer.on("mouseover", function(e) {
 					var tract_id = e.target.feature.id;
 					// Change the style to the highlighted version
 					layer.setStyle(highlightStyle);
@@ -67,7 +67,7 @@ $(function () {
 					popup.appendTo("#map");
 				});
 				// Create a mouseout event that undoes the mouseover changes
-				layer.on("mouseout", function (e) {
+				layer.on("mouseout", function(e) {
 					var tract_id = e.target.feature.id;
 					// Start by reverting the style back
 					layer.setStyle(defaultStyle);
@@ -88,18 +88,18 @@ $(function () {
 		});
 		L.control.scale().addTo(map);
 		console.log(map, '-----------');
-		map.on('moveend load', function () {
+		map.on('moveend load', function() {
 			function constructYelpURL(category_filter) {
 				var URL = "http://api.yelp.com/" +
-					"business_review_search?"+
-					"callback=" + "handleResults" +
-					"&term=" + document.getElementById("term").value + 
-					"&num_biz_requested=10" +
-					"&tl_lat=" + mapBounds.getSouthWest().lat() +
-					"&tl_long=" + mapBounds.getSouthWest().lng() + 
-					"&br_lat=" + mapBounds.getNorthEast().lat() + 
-					"&br_long=" + mapBounds.getNorthEast().lng() +
-					"&ywsid=" + YWSID;
+						"business_review_search?" +
+						"callback=" + "handleResults" +
+						"&term=" + document.getElementById("term").value +
+						"&num_biz_requested=10" +
+						"&tl_lat=" + mapBounds.getSouthWest().lat() +
+						"&tl_long=" + mapBounds.getSouthWest().lng() +
+						"&br_lat=" + mapBounds.getNorthEast().lat() +
+						"&br_long=" + mapBounds.getNorthEast().lng() +
+						"&ywsid=" + YWSID;
 			}
 			console.log(arguments);
 			var map_center = map.getCenter();
@@ -111,27 +111,27 @@ $(function () {
 					category_filter: category_filter
 				},
 				/*
-				url: 'http://data.cityofboston.gov/resource/' + data_id + '.json',
-				data: {
-					'$where': 'within_circle(location,' + map_center.lat + ',' + map_center.lng + ',2000)'
-				},
-				*/
+				 url: 'http://data.cityofboston.gov/resource/' + data_id + '.json',
+				 data: {
+				 '$where': 'within_circle(location,' + map_center.lat + ',' + map_center.lng + ',2000)'
+				 },
+				 */
 				dataType: 'json',
-				success: function (data) {
+				success: function(data) {
 					console.log(data);
 					/*
-					for (var i = 0; i < data.length; i++) {
-						var location = [data[i].location.latitude, data[i].location.longitude];
-						var popup = '';
-						// debugger;
-						for (var k in data[i]) {
-							if (data[i].hasOwnProperty(k)) {
-								popup += k + ': ' + data[i][k] + '<br/>';
-							}
-						}
-						var marker = L.marker(location).addTo(map).bindPopup(popup);
-					}
-					*/
+					 for (var i = 0; i < data.length; i++) {
+					 var location = [data[i].location.latitude, data[i].location.longitude];
+					 var popup = '';
+					 // debugger;
+					 for (var k in data[i]) {
+					 if (data[i].hasOwnProperty(k)) {
+					 popup += k + ': ' + data[i][k] + '<br/>';
+					 }
+					 }
+					 var marker = L.marker(location).addTo(map).bindPopup(popup);
+					 }
+					 */
 				}
 			});
 		});
@@ -147,7 +147,7 @@ $(function () {
 		L.control.layers(baseLayers, overlays).addTo(map);
 	});
 	var census_data = {};
-	$('#filters').on('change', function (e) {
+	$('#filters').on('change', function(e) {
 		var census_var = this.value;
 		$.ajax({
 			url: 'http://api.census.gov/data/2012/acs5',
@@ -158,7 +158,7 @@ $(function () {
 				'get': this.value
 			},
 			dataType: 'json',
-			success: function (data) {
+			success: function(data) {
 				// shift away the header
 				data.shift();
 				for (var i = 0; i < data.length; i++) {
@@ -169,160 +169,160 @@ $(function () {
 		});
 	});
 
-	$('#yelp-business').on('change', function (e) {
+	$('#yelp-business').on('change', function(e) {
 		var category_filter = this.value;
 		$.ajax({
 			url: 'http://api.yelp.com/v2/search?category_filter=' + category_filter
 		})
 	})
 
-	$('#business').on('change', function (e) {
+	$('#business').on('change', function(e) {
 		var data_var = this.value;
 		$.ajax({
 			url: 'http://data.cityofboston.gov/resource/'
 		})
 	});
 	$("#location").autocomplete({
-      source: function( request, response ) {
-        $.ajax({
-          url: "http://api.censusreporter.org/1.0/geo/search",
-          dataType: "json",
-          data: {
-			q: request.term
-          },
-          success: function( data ) {
-			response($.map(data.results, function (item, i) {
-				return {
-					'id': item.full_geoid,
-					'label': item.full_name,
-					'value': item.full_geoid
-				};
-			}));
-          }
-        });
-      },
-      minLength: 3,
-      select: function( event, ui ) {
-		event.preventDefault();
-		$("#location").val(ui.item.label);
-		var geoId = ui.item.value;
-        $.ajax({
-          url: 'http://api.censusreporter.org/1.0/geo/tiger2012/' + geoId,
-          dataType: 'json',
-          data: {
-			geom: true
-          },
-          success: function( data ) {
-			console.log(data);
-			var geoJson = {
-				type: 'FeatureCollection',
-				features: [
-					data
-				]
-			};
-			var fooLayer = L.geoJson().addTo(map);
-			fooLayer.addData(geoJson);
-			map.fitBounds(fooLayer);
-          }
-        });
-		var tableId = 'B01001';
-		$.ajax({
-			url: 'http://api.censusreporter.org/1.0/data/show/latest?table_ids=' + tableId + '&geo_ids=' + geoId,
-			dataType: 'json',
-			success: function( data ) {
-				var tableHtml = "<div id='chart'></div>";
-				var cols = data.tables[tableId].columns;
-				var total;
-				var estimate = data.data[geoId][tableId].estimate;
-				var aggregated = [
-					{ 
-						name: '0-9',
-						male: estimate['B01001003'] +  estimate['B01001004'],
-						female: estimate['B01001027'] + estimate['B01001028'],
-						total: estimate['B01001003'] + estimate['B01001004'] +  estimate['B01001027'] +  estimate['B01001028']
-					},
-					{
-						name: '10-19',
-						male: estimate['B01001005'] +  estimate['B01001006'] +  estimate['B01001007'],
-						female: estimate['B01001029'] +  estimate['B01001030'] +  estimate['B01001031'],
-						total: estimate['B01001005'] +  estimate['B01001006'] +  estimate['B01001007'] +  estimate['B01001029'] +  estimate['B01001030'] +  estimate['B01001031']
-					},
-					{
-						name: '20-29',
-						male: estimate['B01001008'] +  estimate['B01001009'] +  estimate['B01001010'] +  estimate['B01001011'],
-						female: estimate['B01001032'] +  estimate['B01001033'] +  estimate['B01001034'] +  estimate['B01001035'],
-						total: estimate['B01001008'] +  estimate['B01001009'] +  estimate['B01001010'] +  estimate['B01001011'] +  estimate['B01001032'] +  estimate['B01001033'] +  estimate['B01001034'] +  estimate['B01001035']
-					},
-					{
-						name: '30-39',
-						male: estimate['B01001012'] +  estimate['B01001013'],
-						female: estimate['B01001036'] +  estimate['B01001037'],
-						total: estimate['B01001012'] +  estimate['B01001013'] +  estimate['B01001036'] +  estimate['B01001037']
-					},
-					{
-						name: '40-49',
-						male: estimate['B01001014'] +  estimate['B01001015'],
-						female: estimate['B01001038'] +  estimate['B01001039'],
-						total: estimate['B01001014'] +  estimate['B01001015'] +  estimate['B01001038'] +  estimate['B01001039']
-					},
-					{
-						name: '50-59',
-						male: estimate['B01001016'] +  estimate['B01001017'],
-						female: estimate['B01001040'] +  estimate['B01001041'],
-						total: estimate['B01001016'] +  estimate['B01001017'] +  estimate['B01001040'] +  estimate['B01001041']
-					},
-					{
-						name: '60-69',
-						male: estimate['B01001018'] +  estimate['B01001019'] +  estimate['B01001020'] +  estimate['B01001021'],
-						female: estimate['B01001042'] +  estimate['B01001043'] +  estimate['B01001044'] +  estimate['B01001045'],
-						total: estimate['B01001018'] +  estimate['B01001019'] +  estimate['B01001020'] +  estimate['B01001021'] +  estimate['B01001042'] +  estimate['B01001043'] +  estimate['B01001044'] +  estimate['B01001045']
-					},
-					{
-						name: '70-79',
-						male: estimate['B01001022'] +  estimate['B01001023'],
-						female: estimate['B01001046'] +  estimate['B01001047'],
-						total: estimate['B01001022'] +  estimate['B01001023'] +  estimate['B01001046'] +  estimate['B01001047']
-					},
-					{
-						name: '80+',
-						male: estimate['B01001024'] +  estimate['B01001025'],
-						female: estimate['B01001048'] +  estimate['B01001049'],
-						total: estimate['B01001024'] +  estimate['B01001025'] +  estimate['B01001048'] +  estimate['B01001049']
-					}
-				];
-				for ( column in cols ) {
-					for ( var i = 0; i < cols[column].indent; i++ ) {
-						tableHtml += "&nbsp;&nbsp;&nbsp;";
-					}
-					var name = cols[column].name;
-					var count = data.data[geoId][tableId].estimate[column];
-					if ( name.substr(0,5).toLowerCase() === 'total' ) {
-						total = count;
-					}
-					tableHtml += name;
-					tableHtml += ":&nbsp;";
-					tableHtml += count;
-					tableHtml += "<br />";
+		source: function(request, response) {
+			$.ajax({
+				url: "http://api.censusreporter.org/1.0/geo/search",
+				dataType: "json",
+				data: {
+					q: request.term
+				},
+				success: function(data) {
+					response($.map(data.results, function(item, i) {
+						return {
+							'id': item.full_geoid,
+							'label': item.full_name,
+							'value': item.full_geoid
+						};
+					}));
 				}
-				$("#rightsidebar").html( tableHtml );
-				d3.select("#chart")
-					.selectAll("div")
-					.data(aggregated)
-					.enter().append("div")
-					.style("width", function(d) { 
-						return 175 * d.total / total + "%"; 
-					})
-					.text(function(d) { 
-						return d.name; 
-					});
-			}
-		});
-      },
-      open: function() {
-        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
-      },
-      close: function() {
-        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
-      }
-    });
+			});
+		},
+		minLength: 3,
+		select: function(event, ui) {
+			event.preventDefault();
+			$("#location").val(ui.item.label);
+			var geoId = ui.item.value;
+			$.ajax({
+				url: 'http://api.censusreporter.org/1.0/geo/tiger2012/' + geoId,
+				dataType: 'json',
+				data: {
+					geom: true
+				},
+				success: function(data) {
+					console.log(data);
+					var geoJson = {
+						type: 'FeatureCollection',
+						features: [
+							data
+						]
+					};
+					var fooLayer = L.geoJson().addTo(map);
+					fooLayer.addData(geoJson);
+					map.fitBounds(fooLayer);
+				}
+			});
+			var tableId = 'B01001';
+			$.ajax({
+				url: 'http://api.censusreporter.org/1.0/data/show/latest?table_ids=' + tableId + '&geo_ids=' + geoId,
+				dataType: 'json',
+				success: function(data) {
+					var tableHtml = "<div id='chart'></div>";
+					var cols = data.tables[tableId].columns;
+					var total;
+					var estimate = data.data[geoId][tableId].estimate;
+					var aggregated = [
+						{
+							name: '0-9',
+							male: estimate['B01001003'] + estimate['B01001004'],
+							female: estimate['B01001027'] + estimate['B01001028'],
+							total: estimate['B01001003'] + estimate['B01001004'] + estimate['B01001027'] + estimate['B01001028']
+						},
+						{
+							name: '10-19',
+							male: estimate['B01001005'] + estimate['B01001006'] + estimate['B01001007'],
+							female: estimate['B01001029'] + estimate['B01001030'] + estimate['B01001031'],
+							total: estimate['B01001005'] + estimate['B01001006'] + estimate['B01001007'] + estimate['B01001029'] + estimate['B01001030'] + estimate['B01001031']
+						},
+						{
+							name: '20-29',
+							male: estimate['B01001008'] + estimate['B01001009'] + estimate['B01001010'] + estimate['B01001011'],
+							female: estimate['B01001032'] + estimate['B01001033'] + estimate['B01001034'] + estimate['B01001035'],
+							total: estimate['B01001008'] + estimate['B01001009'] + estimate['B01001010'] + estimate['B01001011'] + estimate['B01001032'] + estimate['B01001033'] + estimate['B01001034'] + estimate['B01001035']
+						},
+						{
+							name: '30-39',
+							male: estimate['B01001012'] + estimate['B01001013'],
+							female: estimate['B01001036'] + estimate['B01001037'],
+							total: estimate['B01001012'] + estimate['B01001013'] + estimate['B01001036'] + estimate['B01001037']
+						},
+						{
+							name: '40-49',
+							male: estimate['B01001014'] + estimate['B01001015'],
+							female: estimate['B01001038'] + estimate['B01001039'],
+							total: estimate['B01001014'] + estimate['B01001015'] + estimate['B01001038'] + estimate['B01001039']
+						},
+						{
+							name: '50-59',
+							male: estimate['B01001016'] + estimate['B01001017'],
+							female: estimate['B01001040'] + estimate['B01001041'],
+							total: estimate['B01001016'] + estimate['B01001017'] + estimate['B01001040'] + estimate['B01001041']
+						},
+						{
+							name: '60-69',
+							male: estimate['B01001018'] + estimate['B01001019'] + estimate['B01001020'] + estimate['B01001021'],
+							female: estimate['B01001042'] + estimate['B01001043'] + estimate['B01001044'] + estimate['B01001045'],
+							total: estimate['B01001018'] + estimate['B01001019'] + estimate['B01001020'] + estimate['B01001021'] + estimate['B01001042'] + estimate['B01001043'] + estimate['B01001044'] + estimate['B01001045']
+						},
+						{
+							name: '70-79',
+							male: estimate['B01001022'] + estimate['B01001023'],
+							female: estimate['B01001046'] + estimate['B01001047'],
+							total: estimate['B01001022'] + estimate['B01001023'] + estimate['B01001046'] + estimate['B01001047']
+						},
+						{
+							name: '80+',
+							male: estimate['B01001024'] + estimate['B01001025'],
+							female: estimate['B01001048'] + estimate['B01001049'],
+							total: estimate['B01001024'] + estimate['B01001025'] + estimate['B01001048'] + estimate['B01001049']
+						}
+					];
+					for (column in cols) {
+						for (var i = 0; i < cols[column].indent; i++) {
+							tableHtml += "&nbsp;&nbsp;&nbsp;";
+						}
+						var name = cols[column].name;
+						var count = data.data[geoId][tableId].estimate[column];
+						if (name.substr(0, 5).toLowerCase() === 'total') {
+							total = count;
+						}
+						tableHtml += name;
+						tableHtml += ":&nbsp;";
+						tableHtml += count;
+						tableHtml += "<br />";
+					}
+					$("#rightsidebar").html(tableHtml);
+					d3.select("#chart")
+							.selectAll("div")
+							.data(aggregated)
+							.enter().append("div")
+							.style("width", function(d) {
+								return 175 * d.total / total + "%";
+							})
+							.text(function(d) {
+								return d.name;
+							});
+				}
+			});
+		},
+		open: function() {
+			$(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+		},
+		close: function() {
+			$(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+		}
+	});
 });
